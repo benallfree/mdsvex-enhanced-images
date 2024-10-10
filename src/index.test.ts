320d65ab2b5bd0a9a762ad3e4f6d72c7c3b1ddb6
Ben Allfree <ben@benallfree.com>
2024-10-09 23:07:47 -0700

    Add tests

---
+++ src/index.test.ts
@@ -0,0 +1,59 @@
+import { describe, expect, test } from 'bun:test'
+import { compile } from 'mdsvex'
+import { enhancedImages } from './index'
+
+describe('enhancedImages plugin', () => {
+  test('transforms image nodes to enhanced:img', async () => {
+    const markdown = '![Alt text](image.jpg)'
+    const compiled = await compile(markdown, {
+      remarkPlugins: [[enhancedImages, {}]]
+    })
+
+    expect(compiled.code).toContain('<enhanced:img')
+    expect(compiled.code).toContain(`from "./image.jpg?enhanced"`)
+  })
+
+  test(`path aliases are preserved`, async () => {
+    const markdown = '![Alt text]($images/image.jpg)'
+    const compiled = await compile(markdown, {
+      remarkPlugins: [[enhancedImages, {}]]
+    })
+
+    expect(compiled.code).toContain(`from "$images/image.jpg?enhanced"`)
+  })
+
+  test(`relative paths are preserved`, async () => {
+    const markdown = '![Alt text](../image.jpg)'
+    const compiled = await compile(markdown, {
+      remarkPlugins: [[enhancedImages, {}]]
+    })
+
+    expect(compiled.code).toContain(`from "../image.jpg?enhanced"`)
+  })
+
+  test(`handles frontmatter`, async () => {
+    const markdown = `---
+title: How.This.Blog.Developed
+description: How.This.Blog.Developed
+date: '2024-7-22'
+categories:
+  - sveltekit
+  - optimization
+published: true
+---
+
+## Contents
+
+## General
+
+- 
+- Built using SvelteKit`
+    const compiled = await compile(markdown, {
+      remarkPlugins: [[enhancedImages, {}]]
+    })
+
+    expect(compiled.code).toContain(
+      'export const metadata = {"title":"How.This.Blog.Developed","description":"How.This.Blog.Developed","date":"2024-7-22","categories":["sveltekit","optimization"],"published":true}'
+    )
+  })
+})
