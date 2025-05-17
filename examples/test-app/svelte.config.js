fe4488d9de0855cdcab4e5a7a874ff3181b84e04
Ben Allfree <ben@benallfree.com>
2025-05-17 05:37:06 -0700

    update demo to svelte5

--- examples/test-app/svelte.config.js
+++ examples/test-app/svelte.config.js
@@ -1,14 +1,18 @@
 import adapter from '@sveltejs/adapter-auto'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { mdsvex } from 'mdsvex'
 import { enhancedImages } from 'mdsvex-enhanced-images'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', '.md'],
-  preprocess: mdsvex({
-    extensions: ['.md'],
-    remarkPlugins: [enhancedImages]
-  }),
+  preprocess: [
+    vitePreprocess(),
+    mdsvex({
+      extensions: ['.md'],
+      remarkPlugins: [enhancedImages]
+    })
+  ],
 
   kit: {
     adapter: adapter(),
